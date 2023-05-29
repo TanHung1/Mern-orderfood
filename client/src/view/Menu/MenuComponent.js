8f3d92c6b58038dbdec0f4f2f43d04b431b763d3
tan hung <truongtanhung1910@gmail.com>
2023-05-29 11:47:50 +0700

    filter dishes by category

--- client/src/view/Menu/MenuComponent.js
+++ client/src/view/Menu/MenuComponent.js
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from "react";
 function MenuComponent() {
   const [data, setData] = useState([]);
   const [cartItemCount, setCartItemCount] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  // Thêm state cho "Hiển thị tất cả"
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/product")
@@ -29,29 +32,38 @@ function MenuComponent() {
     const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
     const index = cartItems.findIndex((item) => item._id === product._id);
     if (index !== -1) {
-      cartItems[index].quantity += 1;
+      cartItems[index].quantity++;
     } else {
       cartItems.push({ ...product, quantity: 1 });
     }
     localStorage.setItem("cart", JSON.stringify(cartItems));
-    const itemCount = cartItems.reduce(
-      (count, item) => count + item.quantity,
-      0
-    );
-    setCartItemCount(itemCount);
+    setCartItemCount(cartItemCount + 1);
+  };
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+  // Xử lý sự kiện "Hiển thị tất cả"
+  const handleShowAllClick = () => {
+    setSelectedCategory("");
+    setShowAll(true);
   };
 
   const renders = () => {
-    return data.products?.map((item, index) => {
+    const productsToRender = selectedCategory
+      ? data.products?.filter((item) => item.category === selectedCategory)
+      : data.products || [];
+
+    return productsToRender.map((item, index) => {
       return (
         <div key={index}>
           <div class="card">
             <img class="" src={item.image} alt="Card image cap" />
-
             <div class="card-body">
               <h5 class="card-title">{item.nameprod}</h5>
               <p class="card-text">{item.description}</p>
               <p class="card-price">{item.price}</p>
+              <p class="card-price">{item.category}</p>
               <button
                 class="btn btn-primary"
                 onClick={() => handleAddToCart(item)}
@@ -64,8 +76,37 @@ function MenuComponent() {
       );
     });
   };
+
   return (
     <div>
+      <div className="category-buttons">
+        <button onClick={handleShowAllClick}>All</button>
+        <button
+          className={selectedCategory === "pizza" ? "active" : ""}
+          onClick={() => handleCategoryClick("pizza")}
+        >
+          Pizza
+        </button>
+
+        <button
+          className={selectedCategory === "desserts" ? "active" : ""}
+          onClick={() => handleCategoryClick("desserts")}
+        >
+          Desserts
+        </button>
+        <button
+          className={selectedCategory === "drink" ? "active" : ""}
+          onClick={() => handleCategoryClick("drink")}
+        >
+          Drink
+        </button>
+        <button
+          className={selectedCategory === "side" ? "active" : ""}
+          onClick={() => handleCategoryClick("side")}
+        >
+          Side
+        </button>
+      </div>
       <div className="" style={{ color: "black" }}>
         Sản phẩm hiện đang có trong giỏ hàng: {cartItemCount}
       </div>
