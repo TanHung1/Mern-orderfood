0264a92804c133866c0715eb098c2c9b041d3347
tan hung <truongtanhung1910@gmail.com>
2023-05-30 08:52:02 +0700

    auth google

--- server/src/app/middleware/Authentication.js
+++ server/src/app/middleware/Authentication.js
@@ -1,64 +1,68 @@
-const jwt = require('jsonwebtoken');
-const Account = require('../models/Account');
-const GooglePlusTokenStrategy = require('passport-google-plus-token');
-const passport = require('passport');
+const jwt = require("jsonwebtoken");
+const Account = require("../models/Account");
+const GooglePlusTokenStrategy = require("passport-google-plus-token");
+const passport = require("passport");
 
 const AuthenticationAccount = async (req, res, next) => {
-    const authheader = req.header('Authorization');
-    const accessToken = authheader && authheader.split(" ")[1];
-    if (!accessToken) {
-        return res.status(404).send('Token khong ton tai')
+  const authheader = req.header("Authorization");
+  const accessToken = authheader && authheader.split(" ")[1];
+  if (!accessToken) {
+    return res.status(404).send("Token khong ton tai");
+  }
+
+  console.log(accessToken);
+  try {
+    const record = jwt.verify(accessToken, process.env.jwt_access_token);
+    const user = await Account.findOne({ _id: record.userId });
+
+    if (!record) {
+      return res.status(403).send("Ban khong co quyen truy cap");
     }
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error);
+  }
+};
 
-    console.log(accessToken);
-    try {
-        const record = jwt.verify(accessToken, process.env.jwt_access_token);
-        const user = await Account.findOne({ _id: record.userId });
-
-        if (!record) {
-            return res.status(403).send('Ban khong co quyen truy cap')
-        }
-        req.user = user;
-        next();
-    } catch (error) {
-        res.status(500).send(error)
-        console.log(error)
-    }
-}
-
-passport.use(new GooglePlusTokenStrategy({
-    clientID: '113981226682-vk1qqh65b4d0j2l5ag62k455s69dvkes.apps.googleusercontent.com',
-    clientSecret: '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',
-    // passReqToCallback: true
-}, async (res, req, accessToken, refreshToken, profile, next) => {
-    try {
+passport.use(
+  new GooglePlusTokenStrategy(
+    {
+      clientID:
+        "113981226682-vk1qqh65b4d0j2l5ag62k455s69dvkes.apps.googleusercontent.com",
+      clientSecret: "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",
+      passReqToCallback: true,
+    },
+    async (res, req, accessToken, refreshToken, profile, next) => {
+      try {
         // console.log('accessToken', accessToken);
         // console.log('refreshToken', refreshToken);
         // console.log('profile', profile);
 
         const user = await Account.findOne({
-            authGoogleID: accessToken.id,
-            loginType: 'google'
+          authGoogleID: accessToken.id,
+          loginType: "google",
         });
 
         //Already have an account
-        if(user) return user;
-        
+        if (user) return user;
+
         //no account
         const newUser = new Account({
-            loginType: 'google',
-            authGoogleID: accessToken.id,
-            email: accessToken.emails[0].value
-        })
+          loginType: "google",
+          authGoogleID: accessToken.id,
+          email: accessToken.emails[0].value,
+        });
         await newUser.save();
         // console.log(newUser)
-
-    } catch (error) {
+      } catch (error) {
         console.log(error);
+      }
     }
-}));
-
+  )
+);
 
 module.exports = {
-    AuthenticationAccount
-}
+  AuthenticationAccount,
+};
