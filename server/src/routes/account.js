4d13f1cc0df39ebdeff4b12371ad2d79f13220b3
tan hung <truongtanhung1910@gmail.com>
2023-06-05 20:48:30 +0700

    fix account

--- server/src/routes/account.js
+++ server/src/routes/account.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const AccountControllers = require('../app/controllers/AccountControllers');
+const express = require("express");
+const AccountControllers = require("../app/controllers/AccountControllers");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 // const passportConfig = require('../middleware/passport');
 
-router.post('/register', AccountControllers.register);
-router.post('/login',  AccountControllers.login);
-router.post('/auth/google', passport.authenticate('google-plus-token'), AccountControllers.authGoogle)
-router.post('/update-account',AccountControllers.updateAccount);
+router.post("/register", AccountControllers.register);
+router.post("/login", AccountControllers.login);
+router.post(
+  "/auth/google",
+  passport.authenticate("google-plus-token"),
+  AccountControllers.authGoogle
+);
+router.post("/:id/update-account", AccountControllers.updateAccount);
 
 module.exports = router;
